4852ece42c4f4efec1a12fe7039471c5155b8e7b
Yingjie Yang <102939225+Tiffany678@users.noreply.github.com>
2024-12-05 23:54:03 -0500

    upload assignment

---
+++ C#.NET/Assignment_3/Assignment_3/OOProgram.cs
@@ -0,0 +1,6 @@
+namespace Assignment_3;
+
+public class OOProgram
+{
+    
+}
---
+++ C#.NET/Assignment_3/Assignment_3/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+
+    static void Main(string[] args)
+    {
+        int[] numbers = GeneraleNumbers();
+        Reverse(numbers);
+        PrintNumbers(numbers);
+    }
+
+    private static int[] GeneraleNumbers()
+    {
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i + 1;
+        }
+        return numbers;
+    }
+
+    private static void Reverse(int[] numbers)
+    {
+        int length = numbers.Length/2;
+        for (int i = 0; i < length; i++)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+    }
+
+    private static void PrintNumbers(int[] numbers)
+    {
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number+' ');
+        }
+    }
+}
